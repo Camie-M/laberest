288f2de778880222f7b805884177ed486faf7f83
Camila Moura <camila.miranda.moura@gmail.com>
2020-09-08 15:28:06 -0300

    Criadas pastas de controller das imagens e dos usuários

---
+++ backend/src/controller/ImageController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { ImageInputDTO } from "../model/Image";
+import { ImageDatabase } from "../data/ImageDatabase";
+import { ImageBusiness } from "../business/ImageBusiness";
+import { IdGenerator } from "../services/IdGenerator";
+import { Authenticator } from "../services/Authenticator";
+import { NotFoundError } from "../error/NotFoundError";
+
+export class ImageController {
+  private static ImageBusiness = new ImageBusiness(
+    new ImageDatabase(),
+    new IdGenerator(),
+    new Authenticator()
+  );
+
+  public async createImage(req: Request, res: Response) {
+    try {
+      const token = req.headers.authorization as string;
+
+      const authenticator = new Authenticator();
+      const authenticationData = authenticator.getData(token);
+
+      if (!authenticationData) {
+        throw new Error("Token unanthorized");
+      }
+
+      const userInput: ImageInputDTO = {
+        subtitle: req.body.subtitle,
+        author: req.body.author,
+        file: req.body.file,
+        tags: req.body.tags,
+        collection: req.body.collection,
+      };
+
+      await ImageController.ImageBusiness.createImage(userInput);
+      res.status(200).send("Image created successfully");
+    } catch (error) {
+      res.status(error.errorCode || 400).send({ message: error.message });
+    }
+  }
+
+  public async getImageById(req: Request, res: Response) {
+    try {
+      const userInput: string = req.params.id;
+
+      const result = await ImageController.ImageBusiness.getImageById(
+        userInput
+      );
+
+      if (!result) {
+        throw new NotFoundError("Image not found");
+      }
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(error.errorCode || 400).send({ message: error.message });
+    }
+  }
+}
