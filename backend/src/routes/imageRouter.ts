4b600f70c9b2ac1bbef59423344b1e94c69ad633
Camila Moura <camila.miranda.moura@gmail.com>
2020-09-08 15:40:31 -0300

    Criadas rotas e configurações do servidor

---
+++ backend/src/routes/imageRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { ImageController } from "../controller/ImageController";
+
+export const imageRouter = express.Router();
+
+const imageController = new ImageController();
+
+imageRouter.post("/create", imageController.createImage);
+imageRouter.get("/:id", imageController.getImageById);
